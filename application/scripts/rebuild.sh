47d292a43b878c0354aded761cc7dc676f23c20f
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2017-05-07 15:40:57 +0300

    Updating scripts and Cmake. Adding override to customListener.

--- application/scripts/rebuild.sh
+++ application/scripts/rebuild.sh
@@ -2,11 +2,7 @@
 rm ./CMakeFiles/JWB.dir/depend.make
 rm ./CMakeFiles/JWB.dir/depend.internal
 rm ./CMakeFiles/JWB.dir/CXX.includecache
-rm ./CMakeFiles/JWB.dir/сustomListener.o
-rm ./CMakeFiles/JWB.dir/interfaceDescription.o
-rm ./CMakeFiles/JWB.dir/classDescription.o
-rm ./CMakeFiles/JWB.dir/methodDescription.o
-rm ./CMakeFiles/JWB.dir/metricsCalculator.o
+rm ./CMakeFiles/JWB.dir/*.o
 cp ./temp/depend.make ./CMakeFiles/JWB.dir/
 rm JWB
 make
